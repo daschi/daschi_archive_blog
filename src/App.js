77e158ff8223bee88cc7acb8e39862846dbba296
Daniela Schiano di Cola <daschi87@gmail.com>
2019-10-08 17:09:48 -0700

    Add create-react-app files

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import daniela from './daniela.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={daniela} className='App-image'/>
+        <p>
+          Hi, my name is Daniela.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+// High level sections
+// About
+  // Bio
+    // passions
+    // interests
+    // outside of work
+  // Image + link to github
+  // Technologies I've used recently
+
+// Experience
+
+// Projects
+// Contact
